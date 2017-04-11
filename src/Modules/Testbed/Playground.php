895a43df2f828506b3d95a61f5635d4580cf4c91
Michele Rota <michele.rota@ymail.com>
2017-04-11 15:22:12 +0200

    Pulito il playground

--- src/Modules/Testbed/Playground.php
+++ src/Modules/Testbed/Playground.php
@@ -19,21 +19,16 @@ use Sintattica\Atk\Attributes\EmailAttribute;
 use Sintattica\Atk\Attributes\ExpressionAttribute;
 use Sintattica\Atk\Attributes\FieldSet;
 use Sintattica\Atk\Attributes\FileAttribute;
-use Sintattica\Atk\Attributes\FileWriterAttribute;
 use Sintattica\Atk\Attributes\FlagAttribute;
 use Sintattica\Atk\Attributes\FormatAttribute;
 use Sintattica\Atk\Attributes\HiddenAttribute;
 use Sintattica\Atk\Attributes\HtmlAttribute;
 use Sintattica\Atk\Attributes\IpAttribute;
 use Sintattica\Atk\Attributes\ListAttribute;
-use Sintattica\Atk\Attributes\LiveTextPreviewAttribute;
-use Sintattica\Atk\Attributes\MultipleFileAttribute;
 use Sintattica\Atk\Attributes\NumberAttribute;
 use Sintattica\Atk\Attributes\ParserAttribute;
 use Sintattica\Atk\Attributes\PasswordAttribute;
 use Sintattica\Atk\Attributes\RadioAttribute;
-use Sintattica\Atk\Attributes\RowCounterAttribute;
-use Sintattica\Atk\Attributes\StateAttribute;
 use Sintattica\Atk\Attributes\SwitchAttribute;
 use Sintattica\Atk\Attributes\TextAttribute;
 use Sintattica\Atk\Attributes\TimeAttribute;
@@ -42,241 +37,61 @@ use Sintattica\Atk\Attributes\UpdatedByAttribute;
 use Sintattica\Atk\Attributes\UpdateStampAttribute;
 use Sintattica\Atk\Attributes\UrlAttribute;
 use Sintattica\Atk\Attributes\WeekdayAttribute;
-use Sintattica\Atk\Core\Atk;
 use Sintattica\Atk\Core\Config;
 use Sintattica\Atk\Core\Node;
 use Sintattica\Atk\Relations\ManyToOneRelation;
 use Sintattica\Atk\Relations\OneToManyRelation;
 use Sintattica\Atk\Relations\OneToOneRelation;
 use Sintattica\Atk\Relations\ShuttleRelation;
-use Sintattica\Atk\Utils\EditFormModifier;
 
 class Playground extends Node
 {
-    public function theListAttributeDependency(EditFormModifier $modifier)
-    {
-        $record = &$modifier->getRecord();
-        /*
-        $record['theListAttribute2'] = 5;
-        $modifier->refreshAttribute('theListAttribute2');
-        */
-        //print_r($modifier->getNode());//->getAttribute('theListAttribute2'));
-        $modifier->hideAttribute('theListAttribute2');
-
-        $record = &$modifier->getRecord();
-
-        if ($record['theListAttribute'] == 4 && !$modifier->isInitial()) {
-            $modifier->showAttribute('theListAttribute2');
-            $modifier->refreshAttribute('theListAttribute2');
-        }
-    }
-
-    public function action_updateattributes()
-    {
-        $node = Atk::getInstance()->atkGetNode($this->m_postvars['atknodeuri']);
-        $record = $node->select($this->m_postvars['atkselector'])->getFirstRow();
-
-        foreach (array_keys($node->m_attribList) as $attribname) {
-            $p_attrib = $node->m_attribList[$attribname];
-            if($p_attrib->isPosted($this->m_postvars)) {
-                $record[$p_attrib->fieldName()] = $p_attrib->fetchValue($this->m_postvars);
-            }
-        }
-
-        if($node->updateDb($record)) {
-            $node->getDb()->commit();
-        }
-        exit;
-    }
-
-
     public function __construct($nodeUri)
     {
         parent::__construct($nodeUri, Node::NF_ADD_LINK);
-
         $this->setTable('testbed_Playground');
+
         $attr = $this->add(new Attribute('id', A::AF_AUTOKEY));
         $attr->removeFlag(A::AF_HIDE)->addFlag(A::AF_READONLY);
 
-
-        $this->addSecurityMap('updateattribute', 'edit');
-
-       // $this->setEditableListAttributes(['Attribute', 'BoolAttribute', 'theListAttribute']);
-
-
-        /** tab ************************/
         $tab = 'default';
+        $this->addDefaultFields($tab);
+    }
 
-
-        //test tabbed pane
-
-
-      //  $this->add(new CurrencyAttribute('CurrencyAttribute'), $tab);
-      //  $flags = FileAttribute::AF_FILE_NO_SELECT|FileAttribute::AF_FILE_PHYSICAL_DELETE;
-
-     //   $this->add(new FileAttribute('FileAttribute', $flags, [__DIR__.'/../../../web/files', '/files']));
-
-
-        $attr = $this->add(new Attribute('Attribute'), $tab);
-        $attr->setHelp('attr_help_test');
-        $attr->setPlaceholder('testo di placeholder');
-
-
-        //$this->add(new Attribute('Attribute', A::AF_OBLIGATORY), $tab);
+    protected function addDefaultFields($tab)
+    {
+        $this->add(new Attribute('Attribute'), $tab)->setHelp('attr_help_test')->setPlaceholder('testo di placeholder');
         $this->add(new BoolAttribute('BoolAttribute'), $tab);
         $this->add(new CalculatorAttribute('CalculatorAttribute', 0, '10*5'), $tab);
-
-
-        $attr = new DateTimeAttribute('theDateAttribute', A::AF_SEARCHABLE);
-        $attr->addOnChangeHandler('console.log("theDateTimeAttribute onchange triggered!");');
-        $this->add($attr, 'default');
-
-        // $this->add(new MultiSelectAttribute('MultiSelectAttribute', A::AF_SEARCHABLE, ['option1', 'option2', 'option3'],
-        //     ['option1val', 'option2val', 'option3val']), $tab);
-
-
-        $attr = new ListAttribute('theListAttribute', A::AF_SEARCHABLE,
-            ['testo lungo della option_4', 'option_5', 'testo lungo della option_6'], [4, 5, 6]);
-        $attr->setWidth('300px');
-        $attr->addDependency([$this, 'theListAttributeDependency']);
-        $this->add($attr, "tab2");
-
-        $attr = new ListAttribute('theListAttribute2', A::AF_SEARCHABLE,
-            ['testo lungo della option_4', 'option_5', 'testo lungo della option_6'], [4, 5, 6]);
-        $attr->setWidth('300px');
-        $this->add($attr);
-
-
-        $attr = new ManyToOneRelation('FM2O', A::AF_LARGE | A::AF_SEARCHABLE, $this->getModule().'.m2o_node');
-        //attr->setWidth('300px');
-        //$attr->addDestinationFilter('id = 1');
-        // $attr->setAutoSearch(true);
-        //$attr->addOnChangeHandler('console.log("onchange triggered!");');
-        //$attr->addDependency([$this, 'theListAttributeDependency']);
-        $this->add($attr);
-        $attr->setHelp('prova di help!');
-
-        /*
-        $attr = new MultiSelectListAttribute('theMultiSelectListAttribute', A::AF_SEARCHABLE,
-            ['testo lungo della option_4', 'option_5', 'testo lungo della option_6'], [4, 5, 6]);
-        //$attr->setWidth('300px');
-        $this->add($attr);
-
-
-        $this->add(new TabbedPane('tabbedpane', 0, ['tab1' => ['Attribute'], 'tab2' => ['BoolAttribute', 'CalculatorAttribute', 'theListAttribute2']]));
-
-
-
-
-
-*/
-
-
-
-
-
-// ListAttribute::AF_LIST_OBLIGATORY_NULL_ITEM |
-        $attr = new ListAttribute('theListAttribute2', A::AF_OBLIGATORY | A::AF_SEARCHABLE,
-            ['testo lungo della option_4', 'option_5', 'testo lungo della option_6'], [4, 5, 6]);
-        $this->add($attr, $tab);
-
-        //$this->add(new PasswordAttribute('PasswordAttribute', PasswordAttribute::AF_PASSWORD_NO_VALIDATE, true, ['minnumbers' => 2, 'minalphabeticchars' => 6]), $tab);
-
-
-
-
+        $this->add(new DateTimeAttribute('theDateAttribute', A::AF_SEARCHABLE), $tab);
+        $this->add(new ListAttribute('theListAttribute', A::AF_SEARCHABLE, ['testo lungo della option_4', 'option_5', 'testo lungo della option_6'], [4, 5, 6]), $tab);
+        $this->add(new ManyToOneRelation('FM2O', A::AF_LARGE | A::AF_SEARCHABLE, $this->getModule().'.m2o_node'), $tab);
+        $this->add(new PasswordAttribute('PasswordAttribute', PasswordAttribute::AF_PASSWORD_NO_VALIDATE, true, ['minnumbers' => 2, 'minalphabeticchars' => 6]), $tab);
         $this->add(new OneToManyRelation('the1OneToManyRelation', A::AF_SEARCHABLE, $this->getModule().'.o2m_node', 'playground_id'), $tab);
         $this->add(new OneToManyRelation('the2OneToManyRelation', A::AF_SEARCHABLE, $this->getModule().'.o2m_node2', 'playground_id'), $tab);
-
-
-        for ($i = 0; $i < 10; $i++) {
-
-            $this->add(new ExpressionAttribute('ExpressionAttribute'.$i, A::AF_SEARCHABLE, 'SELECT 5', 'number'), $tab);
-        }
-
-
-        //$tab = 'tab_2';
-
-        $attr = new ListAttribute('theListAttribute', ListAttribute::AF_LIST_OBLIGATORY_NULL_ITEM | A::AF_OBLIGATORY | A::AF_SEARCHABLE,
-            ['option_1', 'option_2', 'option_3'], [1, 2, 3]);
-        $attr->addDependency([$this, 'theListAttributeDependency']);
-        $this->add($attr, $tab);
-
-
-        $attr = new DateTimeAttribute('theDateTimeAttribute', A::AF_SEARCHABLE);
-
-
-        $attr->addOnChangeHandler("console.log(el);");
-
-        $this->add($attr, $tab);
-
-
-        $this->add(new Attribute('Attribute'), $tab);
-        $this->add(new BoolAttribute('BoolAttribute'), $tab);
-        $this->add(new CalculatorAttribute('CalculatorAttribute', 0, '10*5'), $tab);
+        $this->add(new ExpressionAttribute('ExpressionAttribute', A::AF_SEARCHABLE, 'SELECT 5', 'number'), $tab);
         $this->add(new CkAttribute('CkAttribute'), $tab);
-        $this->add(new ColorPickerAttribute('ColorPickerAttribute'), $tab)->setPlaceholder('placeholder')->setHelp('help del colore');
+        $this->add(new ColorPickerAttribute('ColorPickerAttribute'), $tab);
         $this->add(new CountryAttribute('CountryAttribute'), $tab);
         $this->add(new CreatedByAttribute('CreatedByAttribute'), $tab);
         $this->add(new CreateStampAttribute('CreateStampAttribute'), $tab);
-
-
-        /** tab ************************/
-        $tab = "tab_2";
-
-
-
-
         $this->add(new DummyAttribute('DummyAttribute'), $tab);
         $this->add(new DurationAttribute('DurationAttribute'), $tab);
         $this->add(new EmailAttribute('EmailAttribute'), $tab);
-        //$this->add(new ExpressionAttribute('ExpressionAttribute', 0, 'SELECT "test expression"'), $tab);
         $this->add(new IpAttribute('IpAttribute'), $tab);
-
-
-        /** tab ************************/
-        $tab = "tab_3";
-
+        $this->add(new CurrencyAttribute('CurrencyAttribute'), $tab);
         $this->add(new DummyAttribute('FieldSet_dummy1', A::AF_NO_LABEL, 'FieldSet_dummy1_content'), $tab);
         $this->add(new DummyAttribute('FieldSet_dummy2', A::AF_NO_LABEL, 'FieldSet_dummy2_content'), $tab);
         $this->add(new FieldSet('FieldSet', 0, '[FieldSet_dummy1] e [FieldSet_dummy2]'), $tab);
         $this->add(new FileAttribute('FileAttribute', 0, [Config::getGlobal('application_dir').'web/files/', '/files/']), $tab);
-        $this->add(new FileWriterAttribute('FileWriterAttribute', 0, Config::getGlobal('application_dir').'web/files/filewriterfile.txt'), $tab);
         $this->add(new FlagAttribute('FlagAttribute', 0, ['option_with_1', 'option_with_2', 'option_with_4'], [1, 2, 4]), $tab);
         $this->add(new FormatAttribute('FormatAttribute', 0, 'AAA/##/##'), $tab);
-
-        //TODO: FuzzySearchAttribute: test
-        // $this->add(new FuzzySearchAttribute('FuzzySearchAttribute'), $tab);
-
         $this->add(new HiddenAttribute('HiddenAttribute'), $tab);
         $this->add(new HtmlAttribute('HtmlAttribute'), $tab);
-
-
-        /** tab ************************/
-        $tab = "tab_4";
-
-        $this->add(new Attribute('LiveTextPreviewAttributeMaster'), $tab);
-        $this->add(new LiveTextPreviewAttribute('LiveTextPreviewAttribute', 0, 'LiveTextPreviewAttributeMaster'), $tab);
-        $this->add(new MultipleFileAttribute('MultipleFileAttribute', 0, [Config::getGlobal('application_dir').'web/multiplefiles/', '/multiplefiles/']), $tab);
-
         $this->add(new NumberAttribute('NumberAttribute', 0, 20, 2), $tab)->setPlaceholder('test placeholder NumberAttribute');
         $this->add(new ParserAttribute('ParserAttribute', 0, 'NumberAttribute is: [NumberAttribute]'), $tab);
-        $this->add(new PasswordAttribute('PasswordAttribute'), $tab);
         $this->add(new RadioAttribute('RadioAttribute', 0, ['option_1', 'option_2', 'option_3']), $tab);
-
-        /** tab ************************/
-        $tab = "tab_5";
-
-        $this->add(new RowCounterAttribute('RowCounterAttribute'), $tab);
-        $this->add(new StateAttribute('StateAttribute'), $tab);
         $this->add(new SwitchAttribute('SwitchAttribute'), $tab);
-
-        //TODO: TabbedPane: test
-        //$this->add(new TabbedPane('TabbedPane'), $tab);
-
-        //TODO: TagAttribute: test
-        //$this->add(new TagAttribute('TagAttribute'), $tab);
-
         $this->add(new TextAttribute('TextAttribute'), $tab);
         $this->add(new TimeAttribute('TimeAttribute'), $tab);
         $this->add(new TimeZoneAttribute('TimeZoneAttribute'), $tab);
@@ -284,19 +99,7 @@ class Playground extends Node
         $this->add(new UpdateStampAttribute('UpdateStampAttribute'), $tab);
         $this->add(new UrlAttribute('UrlAttribute'), $tab)->setPlaceholder('test placeholder UrlAttribute');
         $this->add(new WeekdayAttribute('WeekdayAttribute'), $tab);
-
-
-        /** RELATIONS **/
-
-        /** tab ************************/
-        $tab = "tab_6";
-
         $this->add(new OneToOneRelation('OneToOneRelation', 0, $this->getModule().'.o2o_node', 'playground_id'), $tab);
-
-
-        $rel = new ShuttleRelation('ShuttleRelation', 0, $this->getModule().'.m2m_node', $this->getModule().'.m2o_node');
-        $rel->setLocalKey('playground_id');
-        $rel->setRemoteKey('remotetable_id');
-        $this->add($rel, $tab);
+        $this->add(new ShuttleRelation('ShuttleRelation', 0, $this->getModule().'.m2m_node', $this->getModule().'.m2o_node', 'playground_id', 'remotetable_id'), $tab);
     }
 }
