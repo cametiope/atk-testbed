4231145df928292e6d7b91de22eec56c2556e36a
Michele Rota <michele.rota@ymail.com>
2017-04-13 16:20:53 +0200

    Initial attemp to DIC

--- web/index.php
+++ web/index.php
@@ -3,27 +3,5 @@
 include_once __DIR__ . '/../vendor/autoload.php';
 
 
-register_shutdown_function( "fatal_handler" );
-
-function fatal_handler() {
-    $errfile = "unknown file";
-    $errstr  = "shutdown";
-    $errno   = E_CORE_ERROR;
-    $errline = 0;
-
-    $error = error_get_last();
-
-    if( $error !== NULL) {
-        $errno   = $error["type"];
-        $errfile = $error["file"];
-        $errline = $error["line"];
-        $errstr  = $error["message"];
-
-        var_dump($error);
-    }
-}
-
-
-
-$atk = new Sintattica\Atk\Core\Atk(getenv('APP_ENV'), __DIR__ . '/../');
-$atk->run();
+$atkBootstrap = new Sintattica\Atk\Core\Atk(getenv('APP_ENV'), __DIR__ . '/../');
+$atkBootstrap->run();
